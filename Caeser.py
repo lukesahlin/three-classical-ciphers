7c5d297e07cbb145326240e14d948ecc79b4ce98
JackKabilAC <jack.kabil@americancementing.com>
2024-09-02 13:49:16 -0700

    First attempt at Caeser. Needs testing

---
+++ Caeser.py
@@ -0,0 +1,38 @@
+def CaeserEncryption(message, key):
+    alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ "
+    result = ""
+
+    # For each char in the message check if the char exists in the alphabet
+    # If it does then get the chars value (index) from the alphabet
+    # Move the index by the key length and then mod by 27 
+    # Add the new char to the result string 
+    for char in message:
+        if char in alphabet:
+            originalChar = alphabet.index(char)
+            newChar = (originalChar + alphabet.index(key) ) % 27
+            result += alphabet[newChar]
+        else:
+            print("Invalid character!")
+            break
+    return result
+
+def CaeserDecryption(message, key):
+    alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ "
+    result = ""
+
+    # For each char in the message check if the char exists in the alphabet
+    # If it does then get the chars value (index) from the alphabet
+    # Move the index by the key length and then mod by 27 
+    # Add the new char to the result string 
+    for char in message:
+        if char in alphabet:
+            originalChar = alphabet.index(char)
+            newChar = (originalChar - alphabet.index(key) ) % 27
+            result += alphabet[newChar]
+        else:
+            print("Invalid character!")
+            break
+    return result
+
+
+
