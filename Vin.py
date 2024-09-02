5e0039d22f8cdda6b5384217ae2bbb5892513037
Mark Sanghera <sanghera.mark@gmail.com>
2024-09-02 14:12:44 -0700

    Created general method and began working on encrpyting method

---
+++ Vin.py
@@ -0,0 +1,14 @@
+def VigenereCipher(message, keyword, encrypt):
+    if encrypt:
+        VigenereCipherEncrypt(message, keyword)
+    else:
+        VigenereCipherDecrypt(message, keyword)
+
+
+def VigenereCipherEncrypt(message, keyword):
+    # check if keyword is smaller than message
+    if len(keyword) < len(message):
+        # make keyword into key
+        for i in len(message):
+            keyword
+        for char in message:
